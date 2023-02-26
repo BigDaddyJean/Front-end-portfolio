f81dd9316223d49ba27a9c0b35e03fe06a28a021
bigdaddyjean <jeannappoleon@pursuit.org>
2023-02-26 14:33:18 -0500

    small correction

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Review from "./review";
+import Review from "./Review";
 import axios from "axios";
 import StarRating from "../StarRating";
 
